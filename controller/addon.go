c70ac396cdab43b6d35ada1b017e711fc84b4a07
Jeff Ortel <jortel@redhat.com>
2022-05-06 05:24:55 -0700

    checkpoint

--- controller/addon.go
+++ controller/addon.go
@@ -2,15 +2,11 @@ package controller
 
 import (
 	"context"
-	"fmt"
 	libcnd "github.com/konveyor/controller/pkg/condition"
-	liberr "github.com/konveyor/controller/pkg/error"
 	"github.com/konveyor/controller/pkg/logging"
 	api "github.com/konveyor/tackle2-hub/k8s/api/tackle/v1alpha1"
 	"github.com/konveyor/tackle2-hub/settings"
-	core "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/storage/names"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -119,53 +115,5 @@ func (r Reconciler) Reconcile(request reconcile.Request) (result reconcile.Resul
 	}
 
 	// Done
-	return
-}
-
-func (r *Reconciler) ensureClaims(addon *api.Addon) (err error) {
-	for _, mount := range addon.Spec.Mounts {
-		if mount.Claim != "" {
-			continue
-		}
-		claim := r.claim(addon, &mount)
-		err := r.Get(
-			context.TODO(),
-			client.ObjectKey{
-				Namespace: claim.Namespace,
-				Name: claim.Name,
-			},
-			claim)
-		if err != nil {
-			if !k8serr.IsNotFound(err) {
-				err = liberr.Wrap(err)
-				return
-			} else {
-				continue
-			}
-		}
-		err = r.Create(context.TODO(), claim)
-		if err != nil {
-			err = liberr.Wrap(err)
-			return
-		}
-	}
-	return
-}
-
-func (r *Reconciler) claim(addon *api.Addon, mount *api.Mount) (claim *core.PersistentVolumeClaim) {
-	claim = &core.PersistentVolumeClaim{
-		ObjectMeta: meta.ObjectMeta{
-			Name:      addon.Name + "-" + mount.Name,
-			Namespace: addon.Namespace,
-		},
-		Spec: core.PersistentVolumeClaimSpec{
-			Resources: *mount.Capacity,
-			StorageClassName: &mount.StorageClass,
-			AccessModes: []core.PersistentVolumeAccessMode{
-				core.ReadWriteMany,
-			},
-		},
-	}
-
 	return
 }
