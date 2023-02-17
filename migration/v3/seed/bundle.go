e30a664bf49c916cd393b46f11004041b6e38947
Jeff Ortel <jortel@redhat.com>
2023-02-17 07:45:27 -0800

    checkpoint

--- migration/v3/seed/bundle.go
+++ migration/v3/seed/bundle.go
@@ -1,58 +1,119 @@
 package seed
 
 import (
-	"encoding/json"
 	"github.com/konveyor/tackle2-hub/model"
+	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 	"os"
 )
 
 //
-// RuleBundle seed object.
-type RuleBundle struct {
-	model.RuleBundle
-	image    string
-	excluded bool
+// Image SVG
+type Image struct {
+	Name string `yaml:"name"`
+	Content string `yaml:"content"`
 }
 
 //
-// Create resources and files.
-func (r *RuleBundle) Create(db *gorm.DB) {
-	r.Image = &model.File{Name: "file.svg"}
+// RuleSet seed object.
+type RuleSet struct {
+	Name string `yaml:"name"`
+	Description string `yaml:"description"`
+	Metadata Metadata `yaml:"metadata"`
+}
+
+func (r *RuleSet) model() (m *model.RuleSet) {
+	return
+}
+
+
+//
+// Metadata windup metadata.
+type Metadata struct {
+	Source string `yaml:"source"`
+	Target string `yaml:"target"`
+}
+
+//
+// RuleBundle seed object.
+type RuleBundle struct {
+	Kind string `yaml:"kind"`
+	Name string `yaml:"name"`
+	Description string `yaml:"description"`
+	Image string `yaml:"image"`
+	RuleSets []RuleSet `yaml:"rulesets"`
+}
+
+//
+// ImgMap image map.
+type ImgMap map[string]string
+
+func (r *RuleBundle) Create(db *gorm.DB, imgMap ImgMap) () {
+	m := r.model()
+	m.Image = &model.File{Name: r.Image + ".svg"}
 	err := db.Create(r.Image).Error
 	if err != nil {
 		return
 	}
-	f, err := os.Create(r.Image.Path)
+	f, err := os.Create(m.Image.Path)
 	if err != nil {
 		return
 	}
 	defer func() {
 		_ = f.Close()
 	}()
-	_, err = f.WriteString(r.image)
+	_, err = f.WriteString(imgMap[imgMap[r.Image]])
 	if err != nil {
 		return
 	}
-	_ = db.Create(&r.RuleBundle)
+	_ = db.Create(m)
 }
 
-//
-// Metadata builds windup metadata.
-func Metadata(source, target string) (b []byte) {
-	type MD struct {
-		Source string `json:"source,omitempty"`
-		Target string `json:"target,omitempty"`
+func (r *RuleBundle) model() (m *model.RuleBundle) {
+	m = &model.RuleBundle{
+		Kind: r.Kind,
+		Name: r.Name,
+		Description: r.Description,
+		RuleSets: []model.RuleSet{},
 	}
-	b, _ = json.Marshal(&MD{
-		Source: source,
-		Target: target,
-	})
+	for _, ruleset := range r.RuleSets {
+		m.RuleSets = append(m.RuleSets, *ruleset.model())
+	}
 	return
 }
 
 //
-// Target builds metadata.
-func Target(t string) (b []byte) {
-	return Metadata("", t)
+// Bundles seed object.
+type Bundles struct {
+	Images []Image `yaml:"images"`
+	Bundles []RuleBundle `yaml:"bundles"`
+}
+
+//
+// Reconcile seeded bundles.
+func (r *Bundles) Reconcile(db *gorm.DB, path string) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return
+	}
+	err = yaml.Unmarshal(b, r)
+	if err != nil {
+		return
+	}
+	imgMap := r.imgMap()
+	for _, bundle := range r.Bundles {
+		bundle.Create(db, imgMap)
+	}
+}
+
+func (r *Bundles) create(db *gorm.DB) (err error) {
+	return
+}
+
+func (r *Bundles) imgMap() (m map[string]string) {
+	m = map[string]string{}
+	for _, img := range r.Images {
+		m[img.Name] = img.Content
+	}
+	return
 }
